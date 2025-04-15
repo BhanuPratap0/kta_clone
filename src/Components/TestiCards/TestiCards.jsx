fe2d43d762f4ed0c602c8e4b8ec4ededfa8a44d5
Bhanu Pratap Singh <119663548+BhanuPratap0@users.noreply.github.com>
2025-04-15 23:55:39 +0530

    Update TestiCards.jsx

--- src/Components/TestiCards/TestiCards.jsx
+++ src/Components/TestiCards/TestiCards.jsx
@@ -6,14 +6,14 @@ const TestiCards = ({ data }) => {
     <div className={styles.TestiCard}>
       <img
         className={styles.QuotationPic}
-        src="./src/Assets/LandingPage/quotation.svg"
+        src="./Assets/LandingPage/quotation.svg"
         alt=""
       />
       <div className={styles.CardDiv}>
         <div className={styles.FirstSection}>
           <img
             className={styles.Stars}
-            src="./src/Assets/LandingPage/stars.svg"
+            src="./Assets/LandingPage/stars.svg"
             alt=""
           />
           <p className={styles.Comment}>{data.comment}</p>
