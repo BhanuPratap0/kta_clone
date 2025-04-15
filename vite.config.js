d33de20bc46001bfb7b59b51b495c115e1a28e88
Bhanu Pratap Singh <119663548+BhanuPratap0@users.noreply.github.com>
2025-04-15 23:45:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PAATH || "./",
+  base: process.env.VITE_BASE_PAATH || "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // Now '@' refers to 'src' folder
