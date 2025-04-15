95a12f808238efd0a1a13b99b4a0fa5f69a817d5
Bhanu Pratap Singh <119663548+BhanuPratap0@users.noreply.github.com>
2025-04-15 23:59:20 +0530

    Update HeroSection.jsx

--- src/Pages/LandingPage/HeroSection/HeroSection.jsx
+++ src/Pages/LandingPage/HeroSection/HeroSection.jsx
@@ -21,7 +21,7 @@ const HeroSection = () => {
       <div className={styles.LandingEnding}>
         <div className="redGradient"></div>
         <img
-          src="./src/Assets/LandingPage/KTABanner.png"
+          src="./Assets/LandingPage/KTABanner.png"
           className={styles.LandingBanner}
           width={"45%"}
           alt=""
